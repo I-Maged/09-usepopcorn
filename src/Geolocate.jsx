324f59aea0dc99169711c5e5d1fdb493e53e4ec1
I-Maged <magedhanafy@gmail.com>
2026-01-29 03:40:28 +0200

    Geolocate

---
+++ src/Geolocate.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { useGeolocate } from './useGeolocate'
+
+const Geolocate = () => {
+  const [countClicks, setCountClicks] = useState(0)
+
+  const {
+    position: { lat, lng },
+    isLoading,
+    error,
+    getPosition,
+  } = useGeolocate()
+
+  const handleClick = () => {
+    setCountClicks((countClicks) => countClicks + 1)
+    getPosition()
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{' '}
+          <a
+            target='_blank'
+            rel='noreferrer'
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  )
+}
+
+export default Geolocate
